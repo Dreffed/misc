75612e229aae21979676ec6190ad1bf8ba546181
dreffed <github@dreffed.com>
2018-09-15 13:26:19 -0700

    Twitter Init

--- search_twitter.py
+++ search_twitter.py
@@ -11,15 +11,16 @@ with open("twitter_credentials.json", "r") as file:
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
 # Create our query
-query = {'q': 'learn python',  
-        'result_type': 'popular',
-        'count': 10,
+query = {'q': 'wow',  
+        'result_type': 'latest',
+        'count': 100,
         'lang': 'en',
         }
 
 # Search tweets
 dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}  
 for status in python_tweets.search(**query)['statuses']:  
+    print('===\n{}\n'.format(json.dumps(status, indent=4)))
     dict_['user'].append(status['user']['screen_name'])
     dict_['date'].append(status['created_at'])
     dict_['text'].append(status['text'])
@@ -28,4 +29,4 @@ for status in python_tweets.search(**query)['statuses']:
 # Structure data in a pandas DataFrame for easier manipulation
 df = pd.DataFrame(dict_)  
 df.sort_values(by='favorite_count', inplace=True, ascending=False)  
-df.head(5)  
+print(df.head(5))
