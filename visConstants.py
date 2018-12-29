af6498fe44a575797c7e1c472a934ac332bc0d1b
David Gloyn-Cox <github@dreffed.com>
2018-12-28 18:11:35 -0800

    Added Visio Constants file

---
+++ visConstants.py
@@ -0,0 +1,16 @@
+vis_paper_sizes = dict([(8,{'name': 'visPaperSizeA3', 'descr':'A3 297 x 420 mm'}),
+(9,{'name': 'visPaperSizeA4', 'descr':'A4 210 x 297 mm'}),
+(11,{'name': 'visPaperSizeA5', 'descr':'A5 148 x 210 mm'}),
+(12,{'name': 'visPaperSizeB4', 'descr':'B4 (JIS) 250 x 354 mm'}),
+(13,{'name': 'visPaperSizeB5', 'descr':'B5 (JIS) 182 x 257 mm'}),
+(14,{'name': 'visPaperSizeFolio', 'descr':'Folio 8 1/2 x 13 in.'}),
+(5,{'name': 'visPaperSizeLegal', 'descr':'Legal 8 1/2 x 14 in.'}),
+(1,{'name': 'visPaperSizeLetter', 'descr':'Letter 8 1/2 x 11 in.'}),
+(18,{'name': 'visPaperSizeNote', 'descr':'Note 8 1/2 x 11 in.'}),
+(24,{'name': 'visPaperSizeSizeC', 'descr':'C size sheet 17 x 22 in.'}),
+(25,{'name': 'visPaperSizeSizeD', 'descr':'D size sheet 22 x 34 in.'}),
+(26,{'name': 'visPaperSizeSizeE', 'descr':'E size sheet 34 x 44 in.'}),
+(0,{'name': 'visPaperSizeUnknown', 'descr':'Unknown'}),])
+
+print(vis_paper_sizes)
+print(vis_paper_sizes[8]['name'])
