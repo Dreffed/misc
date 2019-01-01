f29c776d1015ae42fee243b7d145296966f2e88f
David Gloyn-Cox <github@dreffed.com>
2019-01-01 13:28:45 -0800

    added logger to the program

--- viso_scan.py
+++ viso_scan.py
@@ -5,6 +5,11 @@ import os
 import json
 from datetime import datetime
 from dateutil.parser import parse
+import logging
+from logging.config import fileConfig
+
+fileConfig('logging_config.ini')
+logger = logging.getLogger()
 
 def load_config(config_path):
     if os.path.exists(config_path):
@@ -25,34 +30,61 @@ def process_shape(shape):
     shape_data['name'] = str(shape.Name)
     shape_data['type'] = str(shape.Type)
     shape_data['Text'] = shape.Text
+    logger.debug('({}) - {} [{}]'.format(shape.ID, shape.Name, shape.Type))
 
-    shape_dets = str(shape.Name).split(".")
-    shape_data['name_type'] = shape_dets[0]
-    if len(shape_dets) <= 1:
-        shape_data['subid'] = '0'
-    else:
-        shape_data['subid'] = shape_dets[1]
+    try:
+        shape_dets = str(shape.Name).split(".")
+        shape_data['name_type'] = shape_dets[0]
+        if len(shape_dets) <= 1:
+            shape_data['subid'] = '0'
+        else:
+            shape_data['subid'] = shape_dets[1]
+        try:
+            shape_data['callouts'] = shape.CalloutsAssociated
+            logger.debug('Callouts returned')
+        except:
+            logger.debug('Callouts failed')
 
-    shape_data['callouts'] = shape.CalloutsAssociated
-    if not shape_data['name_type'] == 'Dynamic connector':
-        shape_data['connected_shapes'] = shape.ConnectedShapes(0, "")
-    #else:
-    connected_shapes = shape.connects
-    if len(connected_shapes) > 0:
-        shape_data['connects'] = []
-        for connector in connected_shapes:
-            if connector.FromSheet.Id == shape_data['id']:
-                shape_data['connects'].append({"type": "from", "id": connector.ToSheet.Id})
-            else:
-                shape_data['connects'].append({"type": "to", "id": connector.FromSheet.Id})
-        print('{} -> {}'.format(shape_data['id'], shape_data['connects']))
+        try:
+            shape_data['connected_shapes'] = shape.ConnectedShapes(0, "")
+        except:
+            logger.debug('Connected Shapes failed')
 
-    contained_in = shape.ContainingShape
-    shape_data['containing_shape'] = contained_in.Name
+        try:
+            connected_shapes = shape.connects
+            logger.debug('Connects returned')
+        except:
+            logger.debug('Connects failed')
+            connected_shapes = []
 
-    shape_members = shape.ContainerProperties
-    if shape_members is not None:
-        shape_data['contained_shapes'] = shape_members.GetMemberShapes(16)
+        if len(connected_shapes) > 0:
+            shape_data['connects'] = []
+            for connector in connected_shapes:
+                if connector.FromSheet.Id == shape_data['id']:
+                    shape_data['connects'].append({"type": "from", "id": connector.ToSheet.Id})
+                else:
+                    shape_data['connects'].append({"type": "to", "id": connector.FromSheet.Id})
+        try:
+            contained_in = shape.ContainingShape
+            logger.debug('Containing shapes returned')
+
+            shape_data['containing_shape'] = contained_in.Name
+        except:
+            logger.debug('Containing shapes failed')
+            shape_data['containing_shape'] = None
+
+        try:    
+            shape_members = shape.ContainerProperties
+            logger.debug('Container Properties returned')
+
+            if shape_members is not None:
+                shape_data['contained_shapes'] = shape_members.GetMemberShapes(16)
+        except:
+            logger.debug('Contained shapes failed')
+            shape_data['contained_shapes'] = []
+
+    except Exception as ex:
+        logging.critical(ex)
 
     return shape_data
 
@@ -100,6 +132,7 @@ def process_visiofile(filepath):
                         print("\t{} [{}]\n\t\tError {}".format(shape.Name, shape.Type, ex))
 
                 dwg_file['pages'].append(page_data)
+                logger.info('Processed {} shapes'.format(page_data['shape_count']))
                 
         except Exception as e:
             print("Error {}".format(e))
@@ -115,14 +148,19 @@ def process_visiofile(filepath):
 
 config_path = 'visio_settings.json'
 pickle_file = 'visio_data.pickle'
+logger.debug('Config: {}'.format(config_path))
+logger.debug('Pickle: {}'.format(pickle_file))
+
 cnf_data = load_config(config_path)
+logger.debug(cnf_data)
 
 path = cnf_data['folders']
 filepath = os.path.join(*path)
 filepath = os.path.expanduser(filepath)
-print(filepath)
+logger.info(filepath)
 
 if not os.path.exists(filepath):
+    logger.critical('Missing Path {}'.format(filepath))
     raise 'missing path'
 
 data = {}
@@ -132,7 +170,8 @@ data['files'] = []
 for f in scanFiles(filepath):
     filename, file_extension = os.path.splitext(f['file']) 
     if file_extension == cnf_data['filter']:
-        print(f, filename, file_extension)
+        logger.debug('Filename: {}{}'.format(filename, file_extension))
+
         f_path = os.path.join(f['folder'], f['file'])
         dwg_file = process_visiofile(f_path)
 
